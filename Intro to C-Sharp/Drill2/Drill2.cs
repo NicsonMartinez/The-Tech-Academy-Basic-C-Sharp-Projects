69759663b03d2386b899386d0f57c3e09ef6ac29
Nicson Martinez <martineznicson@gmail.com>
2019-07-09 17:07:29 -0700

    Create Intro to C# files
    Here I am learning the fudamentals of C-Sharp (intro).

---
+++ Intro to C-Sharp/Drill2/Drill2.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Drill1
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to Acme Accounting System");
+        Console.WriteLine("Rememeber, we're \"accounting\" on you!");
+    }
+}
+
