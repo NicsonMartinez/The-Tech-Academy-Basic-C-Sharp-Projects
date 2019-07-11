52663b7d2d495a77d5b0e1edd3535b045d77547f
Nicson Martinez <martineznicson@gmail.com>
2019-07-10 20:53:55 -0700

    Create Boolean Logic C# Intro

---
+++ Intro to C-Sharp/BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("true && false: " + (true && false));
+            Console.WriteLine("true && true: " + (true && true));
+            Console.WriteLine("false && false: " + (false && false));
+
+            Console.WriteLine("\ntrue || true: " + (true || true));
+            Console.WriteLine("true || false: " + (true || false));
+            Console.WriteLine("false || false: " + (false || false));
+
+            Console.WriteLine("\ntrue == true: " + (true == true));
+            Console.WriteLine("true == false: " + (true == false));
+            Console.WriteLine("false == false: " + (false == false));
+
+            Console.WriteLine("\ntrue != true: " + (true != true));
+            Console.WriteLine("true != false: " + (true != false));
+            Console.WriteLine("false != false: " + (false != false));
+
+            //NOTE: If one or the other equals to True but not both.
+            //NOTE: If one or the other equals to False but not both.
+            // X OR Operator
+            Console.WriteLine("\ntrue ^ true: " + (true ^ true));
+            Console.WriteLine("true ^ false: " + (true ^ false));
+            Console.WriteLine("false ^ false: " + (false ^ false));
+
+            Console.WriteLine("\ntrue && true && true && false: " + (true && true && true && false));
+            Console.WriteLine("\ntrue || true || true || false: " + (true || true || true || false));
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
