9870faa8f2350d5406e5e80fb2c19d57cb2d3acf
Nicson Martinez <martineznicson@gmail.com>
2019-07-26 19:12:22 -0700

    Create Drill17 - Void Methods

---
+++ Intro to C-Sharp/Drill17 - Void Methods/Drill17 VoidMethod/Drill17 VoidMethod/OtherClass.cs
@@ -0,0 +1,37 @@
+﻿/*
+EXERCISE:
+IN A CONSOLE APP, CREATE CODE THAT DOES THE FOLLOWING:
+1. Create a class. In that class, create a void method that outputs an integer. Have the method divide the data passed to it by 2.
+
+2. In the Main() method, instantiate that class.
+
+3. Have the user enter a number. Call the method on that number. Display the output to the screen. It should be the entered number, divided by two.
+
+4. Create a method with output parameters.
+
+5. Overload a method.
+
+6. Declare a class to be static.
+*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Drill17_VoidMethod
+{
+    class OtherClass
+    {
+        //Number 1
+        public void DivideBy2(int num)
+        {
+            int result = num / 2;
+            Console.WriteLine(result);
+        }
+
+        //Number 4 & 5
+        public void DivideBy2(int num, out int result)
+        {
+            result = num / 2;
+        }
+    }
+}
---
+++ Intro to C-Sharp/Drill17 - Void Methods/Drill17 VoidMethod/Drill17 VoidMethod/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+EXERCISE:
+IN A CONSOLE APP, CREATE CODE THAT DOES THE FOLLOWING:
+1. Create a class. In that class, create a void method that outputs an integer. Have the method divide the data passed to it by 2.
+
+2. In the Main() method, instantiate that class.
+
+3. Have the user enter a number. Call the method on that number. Display the output to the screen. It should be the entered number, divided by two.
+
+4. Create a method with output parameters.
+
+5. Overload a method.
+
+6. Declare a class to be static.
+*/
+using System;
+
+namespace Drill17_VoidMethod
+{
+    //Number 6
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            //Number 2
+            OtherClass theObject = new OtherClass();
+
+            //Number 3
+            Console.WriteLine("Hello User! Please enter a number");
+            int inputNum = Convert.ToInt32(Console.ReadLine());
+            int result;
+            theObject.DivideBy2(inputNum, out result);
+
+            Console.WriteLine("The number entered, '{0}' divided by 2 is: {1}.", inputNum, result);
+
+        }
+    }
+}
