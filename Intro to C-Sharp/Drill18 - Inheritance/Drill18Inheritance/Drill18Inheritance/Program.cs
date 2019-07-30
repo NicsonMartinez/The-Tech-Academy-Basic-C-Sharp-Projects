5ab8ee42ac5c68629af9137dcc5d68dcdde90136
Nicson Martinez <martineznicson@gmail.com>
2019-07-30 13:45:54 -0700

    Create Drill18 - Inheritance

---
+++ Intro to C-Sharp/Drill18 - Inheritance/Drill18Inheritance/Drill18Inheritance/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Drill18Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+
+            employee.SayName();
+        }
+    }
+}
