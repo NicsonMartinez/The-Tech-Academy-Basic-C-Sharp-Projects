6e705f681dc6eb383922b80a1844763ff8968b1b
Nicson Martinez <martineznicson@gmail.com>
2019-07-31 15:28:07 -0700

    Create Drill19 - Abstract  Classes

---
+++ Intro to C-Sharp/Drill19 - Abstract  Classes/Drill19Abstract/Drill19Abstract/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Drill19Abstract
+{
+    class Employee : Person
+    {
+        public void SayName()
+        {
+            Console.WriteLine("Name: " + firstName + " " + lastName);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Intro to C-Sharp/Drill19 - Abstract  Classes/Drill19Abstract/Drill19Abstract/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+EXERCISE:
+IN A CONSOLE APP, CREATE CODE THAT DOES THE FOLLOWING:
+1. Create an abstract class called Person with two properties: string firstName and string lastName.
+
+2. Give it the method SayName().
+
+3. Create another class called Employee and have it inherit from the Person class.
+
+4. Implement the SayName() method inside of the Employee class.
+
+5. Inside the Main() method, instantiate an Employee object with firstName "Sample" and lastName "Student". Call the SayName() method on the object.
+*/
+
+using System;
+
+namespace Drill19Abstract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.firstName = "Sample";
+            employee.lastName = "Student";
+            employee.SayName();
+        }
+    }
+}
