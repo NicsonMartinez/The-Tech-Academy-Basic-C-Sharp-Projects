76a7b3968c8665ea05c46e433fec1c4bdfca5dea
Nicson Martinez <martineznicson@gmail.com>
2019-08-04 17:34:01 -0700

    Create Drill21 - Operator Overloading

---
+++ Intro to C-Sharp/Drill21 - Operator Overloading/Drill21OpOverlo/Drill21OpOverlo/Employee.cs
@@ -0,0 +1,43 @@
+﻿/*
+EXERCISE:
+IN A CONSOLE APP, CREATE CODE THAT DOES THE FOLLOWING:
+Overload the "==" operator so it checks if two Employee objects are equal by comparing their Id property. 
+*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Drill21OpOverlo
+{
+    class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        
+        public static bool operator== (Employee employee1, Employee employee2)
+        {
+            if (employee1.Id == employee2.Id)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //NOTE: In order for me to overload an operator, '==', I MUST overload operator '!=' .
+        public static bool operator!= (Employee employee1, Employee employee2)
+        {
+            if (employee1.Id != employee2.Id)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
---
+++ Intro to C-Sharp/Drill21 - Operator Overloading/Drill21OpOverlo/Drill21OpOverlo/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+EXERCISE:
+IN A CONSOLE APP, CREATE CODE THAT DOES THE FOLLOWING:
+Overload the "==" operator so it checks if two Employee objects are equal by comparing their Id property. 
+*/
+
+using System;
+
+namespace Drill21OpOverlo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee();
+            Employee employee2 = new Employee();
+
+            employee1.Id = 0001;
+            employee1.Name = "Nicson";
+
+            employee2.Id = 0002;
+            employee2.Name = "Jesse";
+
+            bool isEmployee = employee1 == employee2;
+
+            Console.WriteLine("Does "+ employee1.Name + " and " + employee2.Name + "'s employee Ids match?: " + isEmployee);
+            Console.ReadLine();
+        }
+    }
+}
