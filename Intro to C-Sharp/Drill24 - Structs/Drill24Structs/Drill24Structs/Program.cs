3f618c5fa7fdfc1fe403887d68869e06075664a8
Nicson Martinez <martineznicson@gmail.com>
2019-08-06 01:04:46 -0700

    Create Drill24 - Structs

---
+++ Intro to C-Sharp/Drill24 - Structs/Drill24Structs/Drill24Structs/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+EXERCISE:
+IN A CONSOLE APP, CREATE CODE THAT DOES THE FOLLOWING:
+1. Create a struct called Number and give it the property "Amount" and have it be of data type decimal.
+
+2. In the Main() method, create an object of data type Number and assign an amount to it.
+
+3. Print this amount to the console
+*/
+
+using System;
+
+namespace Drill24Structs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number num = new Number();
+            num.Amount = 45.50m;
+            Console.WriteLine(num.Amount);
+            Console.ReadLine();
+        }
+    }
+
+    struct Number
+    {
+        public decimal Amount { get; set; }
+    }
+}
