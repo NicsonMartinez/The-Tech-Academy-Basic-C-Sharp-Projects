cde8f6c60f6f31b33a6272fc7e3a766123ce11db
Nicson Martinez <martineznicson@gmail.com>
2019-08-06 15:39:02 -0700

    Update TwentyOne files

--- Intro to C-Sharp/TwentyOne/TwentyOne/Program.cs
+++ Intro to C-Sharp/TwentyOne/TwentyOne/Program.cs
@@ -18,25 +18,14 @@ namespace TwentyOne
         static void Main(string[] args)
         {
             Deck deck = new Deck();
+            deck.Shuffle(3);
 
-            int counter = 0;
             foreach (Card card in deck.Cards)
             {
-                if (card.Face == Face.Ace)
-                {
-                    counter++;
-                }
+                Console.WriteLine(card.Face + " of " + card.Suit);
             }
-            Console.WriteLine(counter);
-
-            //deck.Shuffle(3);
-
-            //foreach (Card card in deck.Cards)
-            //{
-            //    Console.WriteLine(card.Face + " of " + card.Suit);
-            //}
-            //Console.WriteLine(deck.Cards.Count);
-            //Console.ReadLine();
+            Console.WriteLine(deck.Cards.Count);
+            Console.ReadLine();
         }
 
     }
